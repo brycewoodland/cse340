8ee08e065d39efbadf41c29b4834cd429b611357
brycewoodland <bwoodlan@byui.edu>
2024-01-31 13:57:20 -0700

    Task 2 Complete

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340bw;
+    OWNER TO cse340131;
 
 -- Table structure for table 'classification'
 CREATE TABLE public.classification (
