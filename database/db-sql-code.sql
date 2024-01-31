13d443f1d42bd736123e786011476c0a5ade9516
brycewoodland <bwoodlan@byui.edu>
2024-01-30 17:16:53 -0700

    Queries 4 & 6 Added

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -236,3 +236,15 @@ VALUES   (
     'White',
     5
   );
+
+-- Query 4
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+-- Query 6
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
