8175f28cf791c056b01aed13506fcf97762599b7
brycewoodland <bwoodlan@byui.edu>
2024-02-03 17:07:59 -0700

    MVC: Get Started

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -7,7 +7,32 @@ CREATE TABLE public.classification (
 	classification_name CHARACTER VARYING NOT NULL,
 	CONSTRAINT classification_pk PRIMARY KEY (classification_id)
 );
-
+INSERT INTO inventory (
+    inv_id,
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id
+  )
+VALUES (
+    inv_id:integer,
+    'inv_make:character varying',
+    'inv_model:character varying',
+    'inv_year:character',
+    'inv_description:text',
+    'inv_image:character varying',
+    'inv_thumbnail:character varying',
+    inv_price:numeric,
+    inv_miles:integer,
+    'inv_color:character varying',
+    classification_id:integer
+  );
 -- Table structure for table 'inventory'
 CREATE TABLE IF NOT EXISTS public.inventory
 (
