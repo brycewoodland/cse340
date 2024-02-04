fa2ca54dce8fd3e2c99bd9706036914f72e3c7c9
brycewoodland <bwoodlan@byui.edu>
2024-02-03 19:17:40 -0700

    MVC: Get Started

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,10 +1,10 @@
-const pool = require("../database/");
+const pool = require("../database/")
 
-/* **************
-* Get all classification data
-* ************** */
-async function getClassifications() {
-    return await pool.query("SELECT * FROM public.classifications ORDER BY classification_name")
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+async function getClassifications(){
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
 module.exports = {getClassifications}
