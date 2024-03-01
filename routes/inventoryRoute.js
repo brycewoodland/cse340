57910414f3d20f2e296a1b6110221f9954f1cf00
brycewoodland <bwoodlan@byui.edu>
2024-03-01 14:49:16 -0700

    Task 3 Complete

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,24 +6,24 @@ const { handleErrors } = require("../utilities");
 const validate = require("../utilities/inventory-validation");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", handleErrors(invController.buildByClassificationId));
 
 // Route to build individual vehicle view
-router.get("/detail/:invId", invController.buildVehicleById);
+router.get("/detail/:invId", handleErrors(invController.buildVehicleById));
 
 // Route to build management view
-router.get("/", invController.buildManage);
+router.get("/", handleErrors(invController.buildManage));
 
 // Route to build add-classification view
-router.get("/add-classification", invController.buildClassification);
+router.get("/add-classification", handleErrors(invController.buildClassification));
 
 // Route to post add-classification view
-router.post('/add-classification', validate.classificationRules(), validate.checkClassificationData, invController.addClassification);
+router.post('/add-classification', validate.classificationRules(), validate.checkClassificationData, handleErrors(invController.addClassification));
 
 // Route to build add-inventory view
-router.get("/add-inventory", invController.buildInventory);
+router.get("/add-inventory", handleErrors(invController.buildInventory));
 
 // Route to post add-inventory view
-router.post("/add-inventory", validate.inventoryRules(), validate.checkInventoryData, invController.addInventory);
+router.post("/add-inventory", validate.inventoryRules(), validate.checkInventoryData, handleErrors(invController.addInventory));
 
 module.exports = router;
